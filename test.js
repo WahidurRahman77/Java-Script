706e613f8a86bd36287e9d029ef65884d3dcd976
Wahidur Rahman <wrrahman456@gmail.com>
2025-06-25 19:19:32 +0600

    Update test.js

--- test.js
+++ test.js
@@ -6,5 +6,14 @@ let gamecounter = 100
 gamecounter++;
 console.log(gamecounter);
 
+let myYoutubename = "mychannel"
+let anotherName = myYoutubename
+anotherName = "not know"
+console.log(anotherName);
+console.log(myYoutubename);
 
+let user = {
+    email : "usermail@gmail.com"
+    uip: "0000.000"
+}
 
